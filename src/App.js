11b2566dd75fd764485e9e229e25bc48c7bb48f3
Tchilo <tchiloross@gmail.com>
2022-02-22 20:26:08 +0200

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 class App extends React.Component {
   constructor() {
-
+    super()
+this.state = {
+  monsters: []
+}
   }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}))
+  }
+  
   render(){
   return (
     <div className="App">
